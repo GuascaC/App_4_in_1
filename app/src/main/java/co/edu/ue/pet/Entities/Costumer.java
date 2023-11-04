d130e2b81e03c501fb1daf04effba7bccf940b0e
CGuasca <mauricioguasca11@gmail.com>
2023-11-03 20:37:03 -0500

    Update

--- app/src/main/java/co/edu/ue/pet/Entities/Costumer.java
+++ app/src/main/java/co/edu/ue/pet/Entities/Costumer.java
@@ -1,37 +1,46 @@
 //Desarrollado por CristianGuasca
 package co.edu.ue.pet.Entities;
 
-import android.location.Address;
-
 import java.io.Serializable;
 
 public class Costumer implements Serializable {
-    //Atributtes
+    // Attributes
     private String name;
     private String last;
     private String address;
     private String selectedFruits;
-    //Constructor
+    private int totalCost;
 
-    public Costumer(String name, String last, String address) {
+    // Constructor
+    public Costumer(String name,String last, String address) {
         this.name = name;
         this.last = last;
         this.address = address;
     }
-    //Info
-    @Override
-    public String toString() {
-        return
-                '\n'+"Name=" + name + " " + last + '\n' +
-                        "Address=" + address + '\n' +
-                        "You buy:"  + selectedFruits + '\n';
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
     }
 
     public String getSelectedFruits() {
         return selectedFruits;
     }
+
     public void setSelectedFruits(String selectedFruits) {
         this.selectedFruits = selectedFruits;
+    }
+
+    // Info
+    @Override
+    public String toString() {
+        return '\n' + "Name=" + name + " " + last + '\n' +
+                "Address=" + address + '\n' +
+                "You buy: " + selectedFruits + '\n' +
+                "Total Cost: $" + totalCost + '\n';
     }
 }
 //Desarrollado por CristianGuasca
--- app/src/main/java/co/edu/ue/pet/Products.java
+++ app/src/main/java/co/edu/ue/pet/Products.java
@@ -2,14 +2,11 @@
 package co.edu.ue.pet;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-
 import co.edu.ue.pet.Entities.Costumer;
 
 public class Products extends AppCompatActivity {
@@ -22,6 +19,14 @@ public class Products extends AppCompatActivity {
     private CheckBox rbPeach;
     private Button btnBuy;
     private Costumer costumer;
+
+    // Prices
+    private static final int PRICE_APPLE = 100;
+    private static final int PRICE_PEAR = 200;
+    private static final int PRICE_BANANA = 2000;
+    private static final int PRICE_PINEAPPLE = 500;
+    private static final int PRICE_GRAPE = 1000;
+    private static final int PRICE_PEACH = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,34 +43,60 @@ public class Products extends AppCompatActivity {
         startActivity(bill);
     }
 
-    //Products
+    // Total
     private void data(View view) {
-        String selectedFruits = "";
+        StringBuilder selectedFruits = new StringBuilder();
         if (rbApple.isChecked()) {
-            selectedFruits += "Apple, ";
+            selectedFruits.append("Apple, ");
         }
         if (rbPear.isChecked()) {
-            selectedFruits += "Pear, ";
+            selectedFruits.append("Pear, ");
         }
         if (rbBanana.isChecked()) {
-            selectedFruits += "Banana, ";
+            selectedFruits.append("Banana, ");
         }
         if (rbPineapple.isChecked()) {
-            selectedFruits += "Pineapple, ";
+            selectedFruits.append("Pineapple, ");
         }
         if (rbGrape.isChecked()) {
-            selectedFruits += "Grape, ";
+            selectedFruits.append("Grape, ");
         }
         if (rbPeach.isChecked()) {
-            selectedFruits += "Peach, ";
+            selectedFruits.append("Peach, ");
         }
-        if (selectedFruits.endsWith(", ")) {
-            selectedFruits = selectedFruits.substring(0, selectedFruits.length() - 2);
+        if (selectedFruits.length() > 0) {
+            selectedFruits.setLength(selectedFruits.length() - 2); // Remove the trailing comma and space
         }
-            costumer.setSelectedFruits(selectedFruits);
-            goBill(costumer);
+        costumer.setSelectedFruits(selectedFruits.toString());
+        int totalCost = calculateTotalCost();
+        costumer.setTotalCost(totalCost);
+        goBill(costumer);
     }
-    //Receive
+
+    private int calculateTotalCost() {
+        int totalCost = 0;
+        if (rbApple.isChecked()) {
+            totalCost += PRICE_APPLE;
+        }
+        if (rbPear.isChecked()) {
+            totalCost += PRICE_PEAR;
+        }
+        if (rbBanana.isChecked()) {
+            totalCost += PRICE_BANANA;
+        }
+        if (rbPineapple.isChecked()) {
+            totalCost += PRICE_PINEAPPLE;
+        }
+        if (rbGrape.isChecked()) {
+            totalCost += PRICE_GRAPE;
+        }
+        if (rbPeach.isChecked()) {
+            totalCost += PRICE_PEACH;
+        }
+        return totalCost;
+    }
+
+    // Receive
     public void receive() {
         Bundle receiveData = getIntent().getExtras();
         if (receiveData != null) {
@@ -83,4 +114,5 @@ public class Products extends AppCompatActivity {
         btnBuy = findViewById(R.id.btnBuy);
     }
 }
+
 //Desarrollado por CristianGuasca
